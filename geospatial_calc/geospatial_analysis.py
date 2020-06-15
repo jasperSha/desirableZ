c16142a3842cac654243b3dbd3663c2ad1e45d8e
jasperSha <jaysha54@gmail.com>
2020-06-15 12:52:59 -0700

    organized data

--- geospatial_calc/geospatial_analysis.py
+++ geospatial_calc/geospatial_analysis.py
@@ -12,17 +12,6 @@ from scipy.spatial import cKDTree
 pd.options.display.max_columns = None
 pd.options.display.max_rows = 10
 
-
-
-os.chdir('/Users/Jasper/Documents/HousingMap/R_data/rental/')
-properties = gpd.read_file('property.gpkg')
-         
-
-os.chdir('/Users/Jasper/Documents/HousingMap/R_data/schools/')
-schools = gpd.read_file('school_package.gpkg')
-
-
-
 def ckdnearest(gdf1, gdf2, gdf2_cols=['gsId']):
     
     nA = np.array(list(zip(gdf1.geometry.x, gdf1.geometry.y)) )
@@ -38,8 +27,25 @@ def ckdnearest(gdf1, gdf2, gdf2_cols=['gsId']):
 
     return gdf
 
-print(ckdnearest(properties, schools))
 
-# print(schools.head())
-# schools = schools.reset_index(drop=True)
-# print(schools.head())
+os.chdir('/home/jaspersha/Projects/HeatMap/GeospatialData/compiled_heatmap_data/')
+
+crime_df = gpd.read_file('fullcrime/full_crimes.shp')
+schools_df = gpd.read_file('greatschools/joined.shp')
+zillow_first_df = gpd.read_file('zillsecondrun/zillsecondrunfirsthalf.shp')
+zillow_second_df = gpd.read_file('zillsecondrun/zillsecondrunsecondhalf.shp')
+lausd_bounds_df = gpd.read_file('lausd/LOS_ANGELES_UNIFIED_SCHOOL.shp')
+
+
+print(crime_df.head(), crime_df.shape)
+print(schools_df.head(), schools_df.shape)
+print(zillow_first_df.head(), zillow_first_df.shape)
+print(zillow_second_df.head(), zillow_second_df.shape)
+print(lausd_bounds_df.head(), lausd_bounds_df.shape)
+
+
+
+
+
+
+
